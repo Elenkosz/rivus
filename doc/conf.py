6a5bdaf15767649d08f084eab5309c4a1f2b3b0c
lnksz <kristof.havasi@tum.de>
2017-08-30 10:43:42 +0200

    Add local readthedocs style. Plus some corrections

--- doc/conf.py
+++ doc/conf.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+# Environment variable to know if the docs are being built on rtd.
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+print
+print("Building on ReadTheDocs: {}".format(on_rtd))
+print
+print("Current working directory: {}".format(os.path.abspath(os.curdir)))
+print("Python: {}".format(sys.executable))
+
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode'
 ]
 
 #templates_path = ['_templates']
@@ -11,16 +23,21 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'rivus'
-copyright = u'2015-2016, ojdo'
+copyright = u'2015-2017, ojdo'
 version = '0.1'
 release = '0.1'
 
 exclude_patterns = ['_build']
 #pygments_style = 'sphinx'
 
+
 # HTML output
 
 htmlhelp_basename = 'rivusdoc'
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # LaTeX output
 
@@ -33,8 +50,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'rivus.tex', u'rivus Documentation',
-   u'ojdo', 'manual'),
+    ('index', 'rivus.tex', u'rivus Documentation',
+     u'ojdo', 'manual'),
 ]
 
 # Manual page output
@@ -53,9 +70,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'rivus', u'rivus  Documentation',
-   u'ojdo', 'rivus', 'A mixed integer linear optimisation model for energy infrastructure networks',
-   'Miscellaneous'),
+    ('index', 'rivus', u'rivus  Documentation',
+     u'ojdo', 'rivus', 'A mixed integer linear optimisation model for energy infrastructure networks',
+     'Miscellaneous'),
 ]
 
 
@@ -65,7 +82,7 @@ texinfo_documents = [
 epub_title = u'rivus'
 epub_author = u'ojdo'
 epub_publisher = u'ojdo'
-epub_copyright = u'2015, ojdo'
+epub_copyright = u'2017, ojdo'
 
 epub_exclude_files = ['search.html']
 
@@ -77,4 +94,3 @@ intersphinx_mapping = {
     'http://docs.python.org/': None,
     'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None),
     'matplotlib': ('http://matplotlib.org/', None)}
-
